e24b79667b0199e068ff0507cc774c45c945f9d0
Raymond Turrisi <raymond.turrisi@gmail.com>
2023-05-08 17:08:37 -0400

    adding tof node

--- src/tof_objects/scripts/tof_objects.py
+++ src/tof_objects/scripts/tof_objects.py
@@ -16,25 +16,22 @@ UDP_PORT = 9002
 d1 = 5.0/1000
 d2 = 4.0/1000
 # Set up UDP socket
-print("Connecting to the socket")
+print("TOF: Connecting to the socket")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((UDP_IP, UDP_PORT))
 sock.listen(1)
 data, addr = sock.accept()
-print("Connected")
+print("TOF: Connected")
 obstacle = [-5,-5]
 heading = 0
 R = PoseStamped()
 
 def obj_positioning(pose_pub):
     global obstacle
-    print("Starting")
     while not rospy.is_shutdown():
         obstacle_detection() # read ToF data to find obstacle x, y coord
-        print("In loop")
         x, y = obstacle
         obstacle_str = f"{x:0.1f},{y:0.1f}"
-        print(f"Obst at: {obstacle_str}")
         pose_pub.publish(obstacle_str)
         
         #rospy.spin()
@@ -50,26 +47,22 @@ def position_callback(msg):
 def obstacle_detection():
     global obstacle
     line = data.recv(1024).decode('UTF-8')
-    print(f"Data b: {line}")
     try:
         #obj_data = json.loads(line)
         raw_data = line.replace('{','').replace('}','').split(',')
         angle = raw_data[0].split(':')[1]
         dist = raw_data[1].split(':')[1]
-        print(f"Angle: {angle}")
-        print(f"Dist: {dist}")
         
         l = float(dist)/1000
         phi = float(angle)
-        print(f"Converted: {l}x{phi}")
+        #print(f"Converted: {l}x{phi}")
         # Maps distance and position to x, y coord of obstacle
         x_obs = R.pose.position.x + d1*cmath.cos(heading) + (d2+l)*cmath.cos(heading+phi)
         y_obs = R.pose.position.y + d1*cmath.sin(heading) + (d2+l)*cmath.sin(heading+phi)
         
         obstacle = [x_obs.real, y_obs.real]
-        print(f"obstacle: {obstacle}")
     except:
-        print("err")
+        print("TOF: Err")
         pass
 
 
